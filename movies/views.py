8837b70bf2f2897b15ac8270a35bb151137c9491
ZUCINNI <alekseenkomp@gmail.com>
2020-01-15 12:40:50 +0300

    first commit 15.01.2020

---
+++ movies/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from movies.models import Movie
+
+
+class MoviesView(View):
+    """List of movies"""
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movies.html', {
+            'movies': movies
+        })

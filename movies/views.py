af063b2462acad039b55fe06411b8ca3c780f206
ZUCINNI <alekseenkomp@gmail.com>
2020-01-26 15:47:19 +0300

    adding ListView and DetailView

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from movies.models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """List of movies"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {
-            'movies': movies
-        })
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = 'movies/movie_list.html'
+
+
+class MovieDetailView(DetailView):
+    """Detail of the Movie"""
+    model = Movie
+    slug_field = 'url'
+
